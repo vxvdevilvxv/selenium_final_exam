e8bf97ce3b66e8356997bafc295a41875ae3e3d0
Vladimir_Gal'binskiy <gal-vova@ya.ru>
2021-09-07 21:05:57 +0300

    final version

--- pages/basket_page.py
+++ pages/basket_page.py
@@ -8,7 +8,7 @@ from selenium.webdriver.support import expected_conditions as EC
 class BasketPage(BasePage):
 
     def should_be_empty(self):
-        assert 'Ваша корзина пуста' in self.browser.find_element(
+        assert 'Your basket is empty' in self.browser.find_element(
             *BasketPageLocators.EMPTY_BASKET).text, 'Basket is not empty'
 
     def items_is_absent(self, timeout=3):
--- test_product_page.py
+++ test_product_page.py
@@ -6,6 +6,7 @@ from .pages.product_page import ProductPage
 from .pages.login_page import LoginPage
 from .pages.basket_page import BasketPage
 
+@pytest.mark.need_review
 class TestUserAddToBasketFromProductPage:
     @pytest.fixture(scope='function', autouse=True)
     def setup(self, browser):
@@ -24,6 +25,7 @@ class TestUserAddToBasketFromProductPage:
         page.open()
         page.should_not_be_success_message()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0'
         page = ProductPage(browser, link)
@@ -32,6 +34,7 @@ class TestUserAddToBasketFromProductPage:
         page.solve_quiz_and_get_code()
         page.equals_title()
         page.equals_price()
+
 
 @pytest.mark.skip
 @pytest.mark.parametrize('promo', range(10))
@@ -43,12 +46,6 @@ def test_guest_can_add_product_to_basket(browser, promo):
     page.equals_title()
     page.equals_price()
     page.should_not_be_success_message()
-#
-# def test_guest_can_go_to_login_page(browser, promo):
-#     link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{}'.format(promo)
-#     page = ProductPage(browser, link)
-#     page.open()
-#     page.start()
 
 
 @pytest.mark.skip
@@ -59,8 +56,6 @@ def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     page.open()
     page.add_to_basket()
     page.should_not_be_success_message()
-
-
 
 
 @pytest.mark.skip
@@ -80,6 +75,7 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.add_to_basket()
     page.should_disappear_of_success_message()
 
+
 @pytest.mark.skip
 def test_guest_should_see_login_link_on_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
@@ -87,7 +83,8 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.open()
     page.should_be_login_link()
 
-@pytest.mark.skip
+
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -96,7 +93,8 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
 
-@pytest.mark.skip
+
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
